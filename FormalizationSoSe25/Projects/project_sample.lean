d428b93c23a38801186873195f9f90b253746f3c
Sadjede11 <sadjedesadat.massudi@stud.uni-greifswald.de>
2025-07-18 12:53:38 +0200

    Update project_sample.lean

--- FormalizationSoSe25/Projects/project_sample.lean
+++ FormalizationSoSe25/Projects/project_sample.lean
@@ -1,24 +1,28 @@
 -- Here is a first `import Mathlib.Tactic` to get things started.
 -- Based on the definitions you need, you can add more imports right below.
 import Mathlib.Tactic
--- Theoretically, you could just write `import Mathlib`, but this will be somewhat slower.
+
 section continuity
-theorem Stetigkeit : ε-δ-Definition der Stetigkeit := by
-  -- Sei ε > 0 beliebig.
-  intro ε ε_pos,
-  -- Da f stetig in l ist, gibt es δ > 0, so dass |x - l| < δ ⇒ |f x - f l| < ε.
-  have f_cont_at_l := h_cont l,
-  specialize f_cont_at_l ε ε_pos,
-  rcases f_cont_at_l with ⟨δ, δ_pos, hδ⟩,
-  -- Da (a n) gegen l konvergiert, gibt es N, so dass für n ≥ N gilt: |a n - l| < δ.
-  have a_conv := h_lim δ δ_pos,
-  rcases a_conv with ⟨N, hN⟩,
-  -- Für n ≥ N gilt dann: |f(a n) - f l| < ε.
-  use N,
-  intros n hn,
-  specialize hN n hn,
-  apply hδ,
-  exact hN,
+
+-- Hier ist eine Lean Definition von Stetigkeit der Abbildung f am Punkt a.
+def stetigkeitdef (f: ℝ → ℝ) : Prop :=  ∀ a : ℝ, ∀ ε > 0, ∃ δ > 0, ∀ x : ℝ, |x - a| < δ → |f x - f a| < ε
+
+def convergenzdef (a : ℕ → ℝ) (L : ℝ) : Prop := ∀ε > 0, ∃ n0 : ℕ, ∀ n ≥ n0, |(a n) - L| < ε
+
+
+theorem stetigconvergent (f: ℝ → ℝ) (a : ℕ → ℝ) ( L : ℝ) (fstetig : stetigkeitdef f) ( aLconvergent : convergenzdef a L) : convergenzdef (fun n => f (a n) (f L) : by
+  intros ε εpos
+  -- Da f stetig in a ist, gibt es δ > 0, sodass |x - L| < δ ⇒ |f x - f L| < ε
+  have h1 := fstetig L ε εpos
+  obtain ⟨δ, δpos, hδ⟩ := exists_prop.mp h1
+  -- Da a → L, gibt es N, sodass |a n - L| < δ für n ≥ N
+  have h2 := aLconvergent δ δpos
+  obtain ⟨N, hN⟩ := exists_prop.mp h2
+  use N
+  intros n hn
+  specialize hN n hn
+  specialize hδ (a n) hN
+  exact hδ
 /- Remember we can open namespaces to shorten names and enable notation.
 
 For example (feel free to change it): -/
